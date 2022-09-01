e4e1e55adc468f039cd4c2b831fe6e5e27616798
sakib151659 <76122348+sakib151659@users.noreply.github.com>
2022-09-01 15:31:32 +0600

    user page modified

---
+++ lib/screens/home/user/que_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QueListPage extends StatefulWidget {
+  const QueListPage({Key? key}) : super(key: key);
+
+  @override
+  State<QueListPage> createState() => _QueListPageState();
+}
+
+class _QueListPageState extends State<QueListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/home/user/user.dart
+++ lib/screens/home/user/user.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:qms/screens/home/user/que_list_page.dart';
 
 import '../../../components/appbar/appbar.dart';
+import '../../../services/auth.dart';
 import '../../../utils/colors_for_app.dart';
 
 class UserPage extends StatefulWidget {
@@ -13,9 +15,51 @@ class UserPage extends StatefulWidget {
 class _UserPageState extends State<UserPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: MyColors.primaryColor,
-      appBar: CustomAppbar.getAppBar(context, "User Panel"),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: MyColors.primaryColor,
+        appBar: AppBar(
+          backgroundColor: Colors.black38,
+          elevation: 0.0,
+          actions: <Widget>[
+
+            TextButton.icon(
+                onPressed: () async {
+                  await AuthService().signOut();
+                },
+                icon: const Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                ),
+                label: const Text("Logout", style: TextStyle(color: Colors.white),)
+            )
+          ],
+
+          flexibleSpace: Container(),
+
+          bottom: TabBar(
+
+            indicatorColor: MyColors.customOrange,
+            indicatorWeight: 4,
+            tabs: [
+              Tab(icon: Icon(Icons.label_important_outlined), text: 'Counter 1',),
+              Tab(icon: Icon(Icons.label_important_outlined), text: 'Counter 2',),
+              Tab(icon: Icon(Icons.label_important_outlined), text: 'Counter 3',),
+            ],
+          ),
+        ),
+
+        body: TabBarView(
+          children: [
+            //MaterialPageBuilder(builder: (context) => personalProfile());
+            QueListPage(),
+            QueListPage(),
+            QueListPage(),
+
+          ],
+        ),
+      ),
     );
   }
 }
