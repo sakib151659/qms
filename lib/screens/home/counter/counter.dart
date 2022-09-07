6e9bcb75589a835cd55b0834dafa70e385baa94a
sakib151659 <76122348+sakib151659@users.noreply.github.com>
2022-09-07 10:42:29 +0600

    couner panel mostly done

--- lib/screens/home/counter/counter.dart
+++ lib/screens/home/counter/counter.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:qms/screens/home/counter/user_list_screen.dart';
 
 import '../../../components/appbar/appbar.dart';
+import '../../../components/custom_text_style/custom_text_style_class.dart';
 import '../../../services/auth.dart';
 import '../../../services/local_storage_manager.dart';
 import '../../../utils/colors_for_app.dart';
@@ -19,6 +21,7 @@ class _CounterPageState extends State<CounterPage> {
   String counterOfficerBranchName = "";
   String counterOfficerCounterNumber = "";
   String userId = FirebaseAuth.instance.currentUser!.uid;
+  bool isRequestedSelected = true;
 
   void _getCounterInfo()async {
     print("userId counter : $userId");
@@ -55,6 +58,102 @@ class _CounterPageState extends State<CounterPage> {
     return Scaffold(
       backgroundColor: MyColors.primaryColor,
       appBar: CustomAppbar.getAppBar(context, "Counter Panel"),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 15,),
+              Text("QMS",
+                style: MyTextStyle.regularStyle4(
+                    fontColor: MyColors.primaryTextColor,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w600
+                ),),
+              Text("Que Management System",
+                style: MyTextStyle.regularStyle4(
+                    fontColor: MyColors.primaryTextColor,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600
+                ),),
+              const SizedBox(height: 20,),
+
+
+              // Tab bar Container
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    flex:1,
+                    child: InkWell(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: isRequestedSelected? MyColors.secondaryColor : Colors.white,
+                          border: isRequestedSelected? Border.all(width: 1, color: MyColors.secondaryColor) : Border.all(width: 1, color: MyColors.secondaryTextColor),
+                        ),
+                        height: 45,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            const SizedBox(width: 5,),
+                            Icon(Icons.list,color: isRequestedSelected?Colors.white :  MyColors.primaryTextColor , size: 18,),
+                            const SizedBox(width: 5,),
+                            Text("Requested",
+                              style: MyTextStyle.regularStyle(fontColor: isRequestedSelected? Colors.white :  MyColors.primaryTextColor, fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ),
+                      onTap: (){
+                        setState(() {
+                          isRequestedSelected = true;
+                        });
+                      },
+                    ),
+                  ),
+
+                  Expanded(
+                    flex: 1,
+                    child: InkWell(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: isRequestedSelected? Colors.white : MyColors.secondaryColor,
+                          border: isRequestedSelected? Border.all(width: 1, color: MyColors.secondaryTextColor) : Border.all(width: 1, color: MyColors.secondaryColor),
+                        ),
+                        height: 45,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            const SizedBox(width: 5,),
+                            Icon(Icons.list_alt,color: isRequestedSelected? MyColors.primaryTextColor : Colors.white, size: 18,),
+                            const SizedBox(width: 5,),
+                            Text("Task",
+                              style: MyTextStyle.regularStyle(fontColor: isRequestedSelected? MyColors.primaryTextColor : Colors.white, fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ),
+                      onTap: (){
+                        setState(() {
+                          isRequestedSelected = false;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+
+              isRequestedSelected?
+              SizedBox(
+                height: 300,
+                  child: UserListScreen(branchName: counterOfficerBranchName, counterNumber: counterOfficerCounterNumber, status: MyTexts.requested,)):
+              SizedBox(
+                height: 300,
+                  child: UserListScreen(branchName: counterOfficerBranchName, counterNumber: counterOfficerCounterNumber, status: MyTexts.approved,)),
+
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/screens/home/counter/user_list_screen.dart
+++ lib/screens/home/counter/user_list_screen.dart
@@ -1,15 +1,69 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+import '../../../components/loading_screen/custom_loading.dart';
+import '../../../utils/colors_for_app.dart';
+import '../../../utils/texts_for_app.dart';
+
 class UserListScreen extends StatefulWidget {
-  const UserListScreen({Key? key}) : super(key: key);
+  final String branchName;
+  final String counterNumber;
+  final String status;
+  const UserListScreen({Key? key,
+    required this.branchName,
+    required this.counterNumber,
+    required this.status
+  }) : super(key: key);
 
   @override
   State<UserListScreen> createState() => _UserListScreenState();
 }
 
 class _UserListScreenState extends State<UserListScreen> {
+  CollectionReference regRef = FirebaseFirestore.instance.collection("queTable");
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder(
+        stream: regRef
+            .where(MyTexts.branchName, isEqualTo: widget.branchName)
+            .where(MyTexts.counterNumber, isEqualTo: widget.counterNumber)
+            .where(MyTexts.status, isEqualTo: widget.status)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if(!snapshot.hasData){
+            //print(snapshot.data.documents.toString());
+
+            return const CustomLoading();
+
+          }
+          return  ListView(
+            children: snapshot.data!.docs.map((document){
+              return Card(
+                color: Colors.black54,
+                child: Column(
+                  children: [
+                    ListTile(
+                      leading:const Icon(Icons.person, size: 30, color: MyColors.customOrange,),
+                      title: Text('\n'+document['email'],
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          )),
+                      subtitle: Text('\n'+document[MyTexts.branchName].toString() +' , ' + document[MyTexts.counterNumber].toString()+'\n',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            //fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            letterSpacing: 2,
+                          )),
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          );
+        }
+    );
   }
 }
